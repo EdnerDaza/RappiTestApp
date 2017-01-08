0ea3f35fcefdcbac512c928f88e504e9cd1b67e0
Edner Enrique Daza Miranda <edner.daza@gmail.com>
2017-01-08 15:25:53 -0500

    Initial commit

---
+++ app/src/main/java/com/ednerdaza/rappi/rappitestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ednerdaza.rappi.rappitestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
